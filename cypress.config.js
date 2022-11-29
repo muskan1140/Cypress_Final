da44670e1023ae672a5f485fe808add0605f1fbb
Danish Soma <danish.soma@karyinfotech.com>
2022-11-29 14:24:31 +0530

    updated the url

--- cypress.config.js
+++ cypress.config.js
@@ -32,7 +32,7 @@ module.exports = defineConfig({
       return config;
     },
     specPattern: "cypress/e2e/features/*.feature",
-    baseUrl: "https://mydoctorportal.herokuapp.com",
+    baseUrl: "https://www.saucedemo.com",
     chromeWebSecurity: false,
   },
 });
